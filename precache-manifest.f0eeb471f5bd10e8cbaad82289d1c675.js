668db85b70299138fbf9191dbb03b0fed1666120
Sebastian Torres Retana <storres2@uw.edu>
2019-04-30 13:45:01 -0700

    Updates

---
+++ precache-manifest.f0eeb471f5bd10e8cbaad82289d1c675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1adbd792fb59c4ddcd4b6961a0f44f",
+    "url": "/iv-dashboard/index.html"
+  },
+  {
+    "revision": "eff3a8a510394e8471d3",
+    "url": "/iv-dashboard/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/iv-dashboard/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "eff3a8a510394e8471d3",
+    "url": "/iv-dashboard/static/js/main.e31657ee.chunk.js"
+  },
+  {
+    "revision": "b32f7ef81b15b8ade03d",
+    "url": "/iv-dashboard/static/js/runtime~main.9cedd0b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/iv-dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
