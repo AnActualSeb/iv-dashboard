31eef40515ed04ad4ed18504e3cefc33754639d0
Sebastian Torres Retana <storres2@uw.edu>
2019-05-09 13:17:58 -0700

    Added CSV drug info and testing basic grid layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Dashboard from './views/Dashboard';
-import DrugScreen from './views/DrugScreen';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Dashboard from "./views/Dashboard";
+import DrugScreen from "./views/DrugScreen";
+import BasicLayout from "./components/BasicLayout";
 // import TopHeader from '../components/topHeader';
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div>
-
           {/*This component being called in router means that it will always be on all of the pages.  Makes things a lot easier.*/}
 
           {/* <TopHeader /> */}
 
           <Route exact path="/" component={Dashboard} />
-          <Route path="/drugScreen" component={DrugScreen} />
-
+          <Route path="/basicLayout" component={BasicLayout} />
         </div>
       </BrowserRouter>
     </div>
--- src/components/drugCard.js
+++ src/components/drugCard.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react';
-import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 import "../styles/drugCard.css";
 
 class DrugCard extends Component {
+  getColor() {}
 
-    getColor() {
-
-    }
-
-    render() {
-        return (
-            <div className='cardContainer'>
-                <Link to={{
-                    pathname: "/drugScreen",
-                    search: "?",
-                    state: this.props
-                }}>
-                    <div className="drugCard">
-                        <div className="drugLabel">
-                            <b>Milrinone</b>
-                        </div>
-                        <div className="content">
-                            <div className="dosage">
-                                <b>0.05</b>
-                                <br />
-                                mcg/kg/hr
-                        </div>
-                            <div className="rate">
-                                <b>0.10</b>
-                                <br />
-                                mL/hr
-                        </div>
-                        </div>
-                        <div className='status'>Active</div>
-                    </div>
-                </Link>
+  render() {
+    return (
+      <div className="cardContainer">
+        <Link
+          to={{
+            pathname: "/basicLayout",
+            search: "?",
+            state: this.props
+          }}
+        >
+          <div className="drugCard">
+            <div className="drugLabel">
+              <b>Milrinone</b>
             </div>
-        );
-    }
+            <div className="content">
+              <div className="dosage">
+                <b>0.05</b>
+                <br />
+                mcg/kg/hr
+              </div>
+              <div className="rate">
+                <b>0.10</b>
+                <br />
+                mL/hr
+              </div>
+            </div>
+            <div className="status">Active</div>
+          </div>
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default DrugCard;
