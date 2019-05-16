fe28a7b45109b1a85f819dba5d3159979bf210eb
Sam O'Brien <samcob@uw.edu>
2019-05-15 17:11:40 -0700

    top header and iv pump brain

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import DrugCard from '../components/drugCard';
 import '../styles/dashboard.css';
+import TopHeader from '../components/topHeader';
 import data from "../data/drugData.json";
+import pumpBrain from '../images/pumpBrain.png';
 
 class Dashboard extends Component {
     makeCards() {
         var arr = []
+        var counter = 2;
         for (var i = 0; i < data.length; i++) {
+            counter++;
             arr.push(< DrugCard key={i} drugName={data[i]["Drug Name"]} dosage={data[i]["Dosage"]} dosageUnit={data[i]["Dosage Unit"]} rate={data[i]["Rate"]} rateUnit={data[i]["Rate Unit"]} location={data[i]["Brain"] + data[i]["Pump"]} color={data[i]["Drug Color Code"]} method={data[i]["Method"]} />)
-            //if statement to get relevant information
+            if ((counter % 4) === 0) {
+                arr.push(<img className='pumpBrain' src={pumpBrain} alt="Pump Brain"></img>)
+            }
         }
         return arr;
     }
@@ -17,6 +23,7 @@ class Dashboard extends Component {
     render() {
         return (
             <div className='dashboard'>
+                <TopHeader />
                 {this.makeCards()}
             </div>
 
